7be799d202d6f1fc149f417546c838e4a6d38877
minadan77 <96655942+minadan77@users.noreply.github.com>
2024-07-21 18:20:14 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', (event) => {
             return cache.addAll([
                 '/',
                 '/index.html',
-                '/icon.png',  // Asegúrate de que este archivo exista
+                '/icon.png', // Asegúrate de que este archivo existe
                 '/manifest.json',
                 '/service-worker.js'
             ]).catch((error) => {
